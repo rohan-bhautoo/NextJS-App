39c45c0eddfd4240b9281c2875b302763f318dcd
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-25 21:28:46 +0400

    Handling Unexpected Errors

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>The requested page doesn't exists.</div>;
+  return <div>The requested page doesn&apos;t exists.</div>;
 };
 
 export default NotFoundPage;
