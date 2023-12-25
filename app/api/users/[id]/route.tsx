0465e944e29ea8c5006b2587c0a70baf5f045669
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-25 22:50:07 +0400

    Fetching a Single Object

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+  params: { id: number };
+}
+
+export function GET(request: NextRequest, { params }: Props) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: "Rohan" });
+}
