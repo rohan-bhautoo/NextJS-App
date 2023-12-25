badc84cfe3ad8fb940b4ac4e6acb20994455f570
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-25 23:07:20 +0400

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,3 +10,15 @@ export function GET(request: NextRequest, { params }: Props) {
 
   return NextResponse.json({ id: 1, name: "Rohan" });
 }
+
+export async function PUT(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: body.id, name: body.name, isUpdated: true });
+}
