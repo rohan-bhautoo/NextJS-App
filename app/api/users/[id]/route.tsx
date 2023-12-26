5c3a873e020a4223005d0e1e370ec297a2851908
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-26 11:47:19 +0400

    Fetching Data with Prisma

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,15 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
 interface Props {
-  params: { id: number };
+  params: { id: string };
 }
 
-export function GET(request: NextRequest, { params }: Props) {
-  if (params.id > 10)
+export async function GET(request: NextRequest, { params }: Props) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  return NextResponse.json({ id: 1, name: "Rohan" });
+  return NextResponse.json(user);
 }
 
 export async function PUT(request: NextRequest, { params }: Props) {
@@ -20,15 +27,15 @@ export async function PUT(request: NextRequest, { params }: Props) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  if (params.id > 10)
+  /*if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
-
+*/
   return NextResponse.json({ id: body.id, name: body.name, isUpdated: true });
 }
 
 export function DELETE(request: NextRequest, { params }: Props) {
-  if (params.id > 10)
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  /*if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });*/
 
   return NextResponse.json({});
 }
