25e5eecad5f9ca4ee65739a3c9ddc8cd8b3a823a
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-26 16:06:41 +0400

    Showing Uploaded Images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,34 @@
 "use client";
-import { CldUploadWidget } from "next-cloudinary";
-import React from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+import React, { useState } from "react";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
   return (
-    <CldUploadWidget uploadPreset="dsrqynhc">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="Test" />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+        uploadPreset="dsrqynhc"
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
