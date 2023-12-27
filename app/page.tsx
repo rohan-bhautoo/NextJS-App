eb8bc63eecc8ceb82ae35099d036333b0a8428a3
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-27 17:51:11 +0400

    Optimizing Images + Remote Images

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import blackHole from "@/public/images/Space-Stars-Black-Hole.jpg";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        className="object-cover"
+        alt="Black Hole"
+        fill
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+      />
     </main>
   );
 }
