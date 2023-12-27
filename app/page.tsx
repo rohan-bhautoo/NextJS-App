6c9853f22ac3f1aeada099d37b808e17b4996314
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-27 18:34:00 +0400

    Fixed Import

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import HeavyComponent from "./components/HeavyComponent";
-
 export default async function Home() {
   return (
     <main className="relative h-screen">
       <h1>Hello World</h1>
-      <HeavyComponent />
     </main>
   );
 }
